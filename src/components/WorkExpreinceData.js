cb5856086a964accf6676b01691a45eb7d8a3ac8
Buğra Şenocak <89106419+bugrasenocak@users.noreply.github.com>
2021-08-20 15:09:16 +0300

    Change for Work Experience

--- src/components/WorkExpreinceData.js
+++ src/components/WorkExpreinceData.js
@@ -1,22 +1,22 @@
 export const WorkExpreinceData = [
   {
-    title: 'Yapi Kredi',
-    role: 'Young Branch Intern',
+    title: "Yapi Kredi",
+    role: "Young Branch Intern",
     image:
-      'https://media-exp1.licdn.com/dms/image/C4E0BAQFIa6RI3F-P-Q/company-logo_200_200/0/1605083987683?e=1637193600&v=beta&t=TRncGGJpv8OZS09KoQo4k2_hcnouFiO27Ndy3GgfGcw',
+      "https://media-exp1.licdn.com/dms/image/C4E0BAQFIa6RI3F-P-Q/company-logo_200_200/0/1605083987683?e=1637193600&v=beta&t=TRncGGJpv8OZS09KoQo4k2_hcnouFiO27Ndy3GgfGcw",
     about_role:
-      "I'm working remotely. I was involved in case work within the scope of the Internship Program. Executive seminars, Branch Segment Trainings, Webinars, Online TrainingPackages.",
-    cName: 'p-4',
-    begin_date: 'June 2021',
-    end_date: 'August 2021',
+      "I'm working remotely. I was involved in case work within the scope of the Internship Program. Executive seminars, Branch Segment Trainings, Webinars and Online Training Packages.",
+    cName: "p-4",
+    begin_date: "June 2021",
+    end_date: "August 2021",
   },
   {
-    title: 'Erzincan University',
-    role: 'Intern',
+    title: "Erzincan University",
+    role: "Intern",
     about_role:
-      'In 2019, I worked in the university student program for 3 months within the scope of ISKUR on-the-job training program. I tried to help civil servants and academics as part of the student program.',
-    cName: 'p-4',
-    begin_date: 'June 2019',
-    end_date: 'September 2019',
+      "In 2019, I worked in the university student program for 3 months within the scope of ISKUR on-the-job training program. I tried to help civil servants and academics as part of the student program.",
+    cName: "p-4",
+    begin_date: "June 2019",
+    end_date: "September 2019",
   },
 ];
